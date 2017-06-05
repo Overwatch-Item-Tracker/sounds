cf0564a98e836dd69d18947e2dc58b62ff50ec8e
Js41637 <js41637@gmail.com>
2017-06-05 21:50:36 +1000

    M

--- scripts/generateCustomData.js
+++ scripts/generateCustomData.js
@@ -15,7 +15,7 @@ for (let hero in existingCustomSounds) {
 
 const newTime = Date.now()
 
-const dirs = ['unknown', 'athena', 'hollywood-guy', 'lucioball', 'snowball-offensive', 'uprising', 'junkenstein']
+const dirs = ['unknown', 'athena', 'hollywood-guy', 'lucioball', 'snowball-offensive', 'uprising', 'junkenstein', 'random']
 
 const out = {}
 dirs.forEach(thing => {
