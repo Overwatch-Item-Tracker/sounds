ae1ab5e76b20823c219aca95611757bb6ebc804d
Js41637 ftw <js41637@gmail.com>
2017-07-12 16:47:49 +1000

    New sounds

--- scripts/generateIDs.js
+++ scripts/generateIDs.js
@@ -3,9 +3,12 @@ const sounds = require('../data/soundFiles.json')
 const moreSounds = require('../data/customSounds.json')
 const evenMoreSounds = require('../data/03F.json')
 
+const outputEvenMoreSounds = false
+
 const soundIDs = {}
 
 for (let hero in sounds) {
+	if (hero === '01B') continue
   for (let type in sounds[hero]) {
     for (let sound in sounds[hero][type]) {
       soundIDs[sound] = true
@@ -21,10 +24,12 @@ for (let hero in moreSounds) {
   }
 }
 
-for (let type in evenMoreSounds) {
-  for (let sound of evenMoreSounds[type]) {
-    soundIDs[sound.id] = true
-  }
+if (outputEvenMoreSounds) {
+	for (let type in evenMoreSounds) {
+		for (let sound of evenMoreSounds[type]) {
+			soundIDs[sound.id] = true
+		}
+	}
 }
 
 fs.writeFileSync('../data/ids.json', JSON.stringify(soundIDs))
